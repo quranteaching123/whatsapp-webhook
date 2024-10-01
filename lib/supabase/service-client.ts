20b1c9655da64be59d852752fa3f401db1a9da0b
quranteaching123 <57406421+quranteaching123@users.noreply.github.com>
2024-10-01 16:04:52 +0500

    Update service-client.ts

--- lib/supabase/service-client.ts
+++ lib/supabase/service-client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
 function createServiceClient() {
-    return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE)
+    return createClient('https://xpoejfumaendasjposol.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhwb2VqZnVtYWVuZGFzanBvc29sIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNzc3NTY2NywiZXhwIjoyMDQzMzUxNjY3fQ.AJ7Du79RWTxQ0WAQAnos8edyhfhJPXdqT2rbRlRWFCI')
 }
 
 export {
